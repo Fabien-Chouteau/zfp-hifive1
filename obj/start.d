4005960c2979d4e421cce84ef37bcba92bf77198
Fabien Chouteau <fabien.chouteau@gmail.com>
2018-01-20 19:54:17 +0100

    Update the run-time for 7.2.0

--- obj/start.d
+++ obj/start.d
@@ -1,1 +1,1 @@
-start.o: /home/chouteau/src/github/zfp-hifive1/arch/start.S 20170211214844
+start.o: /home/chouteau/src/github/freedom-e-sdk/Ada_Drivers_Library/examples/HiFive1/zfp-hifive1/arch/start.S 20180120184059
